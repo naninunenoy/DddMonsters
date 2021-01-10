c4ee03c3d1ddfdd367ad9e2af685e32c1ac7ca95
naninunenoy <naninuneno0411@gmail.com>
2021-01-11 02:58:45 +0900

    タイプ抜け漏れのテスト追加

---
+++ DddMonsters/Assets/DddMonsters/Scripts/Tests/EditMode/TypeRelationshipCalculatorTest.cs
@@ -0,0 +1,19 @@
+﻿using DddMonsters.Domains;
+using NUnit.Framework;
+
+namespace DddMonsters.Tests.EditMode {
+    public class TypeRelationshipCalculatorTest {
+        [Test]
+        public void タイプの漏れがないかテスト() {
+            foreach (Type x in System.Enum.GetValues(typeof(Type))) {
+                foreach (Type y in System.Enum.GetValues(typeof(Type))) {
+                    try {
+                        _ = TypeRelationshipCalculator.Calculate(x, y);
+                    } catch (System.ArgumentOutOfRangeException e) {
+                        Assert.Fail(e.Message);
+                    }
+                }
+            }
+        }
+    }
+}
