0674280f9fe5ec68602549b946700fbd269b2270
naninunenoy <naninuneno0411@gmail.com>
2021-01-17 23:05:06 +0900

    適当ダメージ計算

--- DddMonsters/Assets/DddMonsters/Scripts/Domains/Battle/DamageCalculator.cs
+++ DddMonsters/Assets/DddMonsters/Scripts/Domains/Battle/DamageCalculator.cs
@@ -1,8 +1,8 @@
 ﻿namespace DddMonsters.Domains {
     public static class DamageCalculator {
         public static Damage Calculate(DamageParams offense, DamageParams defense) {
-            // ToDo
-            return new Damage(-1);
+            // ToDo 一旦技の威力がそのままダメージ
+            return new Damage(offense.move.power.value);
         }
     }
 }
--- DddMonsters/Assets/DddMonsters/Scripts/Tests/EditMode/DamageTest.cs
+++ DddMonsters/Assets/DddMonsters/Scripts/Tests/EditMode/DamageTest.cs
@@ -1,16 +1,19 @@
 using NUnit.Framework;
 using DddMonsters.Domains;
 
-namespace DddMonsters.Tests.EditMode
-{
-    public class DamageTest
-    {
+namespace DddMonsters.Tests.EditMode {
+    public class DamageTest {
+        MonsterProperties ofnProp = new MonsterProperties(new HitPoint(100));
+        MonsterMove ofnMove = new MonsterMove(new MonsterMovePower(100), Type.Normal);
+        MonsterProperties defProp = new MonsterProperties(new HitPoint(100));
+        MonsterMove defMove = new MonsterMove(new MonsterMovePower(100), Type.Normal);
+
         [Test]
         public void DamageTest0() {
-            var offence = new DamageParams();
-            var defence = new DamageParams();
+            var offence = new DamageParams(ofnProp, ofnMove);
+            var defence = new DamageParams(defProp, defMove);
             var dmg = DamageCalculator.Calculate(offence, defence);
-            Assert.That(dmg.value, Is.EqualTo(0));
+            Assert.That(dmg.value, Is.EqualTo(100));
         }
     }
 }
